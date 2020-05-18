9193284dbd1ccf2ba1dbfc4b6ca19aa25300b345
oomtball <b05705051@ntu.edu.tw>
2020-05-18 11:35:24 +0800

    update record

--- src/views/record/index.vue
+++ src/views/record/index.vue
@@ -7,10 +7,26 @@
         <div class="record_left">
           <div><h1 style="font-size: 35px; font-family:Microsoft JhengHei; text-align: center; margin: 20px;">歷史考試紀錄</h1></div>
           <div class="homepage_line_color2"></div>
+          <div style="background-color : #4A4E69;">
+            <h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 20px; color: white">．考試一</h3>
+          </div>
+          <div><h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 20px;">．考試二</h3></div>
         </div>
       </el-col>
-      <el-col :span="12">
-        <div class="record_right"></div>
+      <el-col :span="2">
+        <div class="record_mid"></div>
+      </el-col>
+      <el-col :span="10">
+        <div class="record_right1">
+          <div><h1 style="font-size: 35px; font-family:Microsoft JhengHei; text-align: center; margin: 20px;">該次考試單字</h1></div>
+          <h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 10px;"><i class="el-icon-close"></i>apple</h3>
+          <h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 10px;"><i class="el-icon-check"></i>banana</h3>
+        </div>
+        <div class="record_right2">
+          <div><h1 style="font-size: 35px; font-family:Microsoft JhengHei; text-align: center; margin: 20px;">歷史考試紀錄</h1></div>
+          <h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 10px;">．apple : 100%</h3>
+          <h3 style="font-size: 25px; font-family:Microsoft JhengHei; text-align: left; margin: 10px;">．banana : 50%</h3>
+        </div>
       </el-col>
     </el-row>
   </div>
@@ -33,14 +49,25 @@ export default {
 }
 .record_left {
   background-color: #F2E9E4
-  height:500px
+  height:600px
   margin-top:40px
   overflow : auto
 }
-.record_right {
+.record_mid {
   background-color: white
-  height:500px
+  height:600px
   margin-top:40px
+  overflow : auto
+}
+.record_right1 {
+  background-color: white
+  height:300px
+  margin-top:40px
+  overflow : auto
+}
+.record_right2 {
+  background-color: #F2E9E4
+  height:300px
   overflow : auto
 }
 .homepage_line_color2 {
