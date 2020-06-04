c628615b128cd893ebd22b0a50c100dda3f87bd7
B05705055 <46625261+B05705055@users.noreply.github.com>
2020-06-04 16:07:36 +0800

    addword

--- frontend/src/views/addWord/index.vue
+++ frontend/src/views/addWord/index.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <div style="margin-bottom: 50px;">
     <el-row>
       <el-col :span="12">
 	    <div><h1 style="font-size: 40px; font-family:Microsoft JhengHei; padding-left: 50px;">Word</h1></div>
@@ -13,9 +12,7 @@
           <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">小卡名稱</h1></div>
 		  <div>
 		  <el-input
-		    placeholder="请输入内容"
-		    v-model="word1"
-		    clearable>
+		    placeholder="请输入内容" v-model="cardname" clearable>
 		  </el-input>
 		</div>
       </el-col>
@@ -23,60 +20,36 @@
         <div><div><h1 style="font-size: 20px; font-family:Microsoft JhengHei; padding-left: 50px;"></h1></div></div>
       </el-col>
     </el-row>
-	<el-row style="width: 70%; margin:0px auto; margin-top: 30px">
-      <el-col :span="10">
+	
+	<div style="margin-bottom: 50px;">
+	  <el-row v-for="(id, index) in addWordList" :key="index" style="width: 70%; margin:0px auto; margin-top: 30px">
+        <el-col :span="10">
           <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">單字</h1></div>
 		  <div>
-		  <el-input
-		    placeholder="请输入内容"
-		    v-model="word1"
-		    clearable>
-		  </el-input>
-		</div>
-      </el-col>
-	  <el-col :span="4"><div><br></div></el-col>
-      <el-col :span="10">
-	    <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">中文</h1></div>
-        <div>
-		  <el-input
-		    placeholder="请输入内容"
-		    v-model="word1"
-		    clearable>
-		  </el-input>
-		</div>
-      </el-col>
-    </el-row>
-	<el-row style="width: 70%; margin:0px auto; margin-top: 30px">
-      <el-col :span="10">
-          <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">單字</h1></div>
-		  <div>
-		  <el-input
-		    placeholder="请输入内容"
-		    v-model="word1"
-		    clearable>
-		  </el-input>
-		</div>
-      </el-col>
-	  <el-col :span="4"><div><br></div></el-col>
-      <el-col :span="10">
-	    <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">中文</h1></div>
-        <div>
-		  <el-input
-		    placeholder="请输入内容"
-		    v-model="definition1"
-		    clearable>
-		  </el-input>
-		</div>
-      </el-col>
+		    <el-input placeholder="请输入内容" v-model="id.word" clearable></el-input>
+		  </div>
+        </el-col>
+	    <el-col :span="4"><div><br></div></el-col>
+        <el-col :span="10">
+	      <div><h1 style="font-size: 20px; font-family:Microsoft JhengHei;">中文</h1></div>
+          <div>
+		    <el-input placeholder="请输入内容" v-model="id.definition" clearable></el-input>
+		  </div>
+        </el-col>
 	  
-    </el-row>
+      </el-row>
+	</div>
+	
+	<div style="margin-top: 50px; margin-bottom: 150px;">
+	  <el-button type="info" style="position:absolute; right:17%; font-size: 20px; font-family:Microsoft JhengHei; font-weight: bold;" @click="addRow()" icon="el-icon-plus" circle></el-button>
 	</div>
 	
 	<div class="homepage_line_color" style="margin:0px auto; width: 70%"></div>
 	
 	<div style="margin-top: 50px;">
+	  
 	  <router-link to="/Word" style="text-decoration:none;">
-	  <el-button type="info" style="position:absolute; right:17%; font-size: 20px; font-family:Microsoft JhengHei; font-weight: bold" icon="el-icon-plus" circle></el-button>
+	  <el-button type="info" style="position:absolute; right:17%; font-size: 20px; font-family:Microsoft JhengHei; font-weight: bold" round>新增小卡</el-button>
 	  </router-link>
 	</div>
 	
@@ -94,10 +67,15 @@ export default {
   data(){
 	return {
 	  cardname: "",
-	  word1: "",
-	  word2: "",
-	  definition1: "",
-	  definition2: ""
+	  addWordList: [
+	    {word: "", definition: ""},
+		{word: "", definition: ""}
+	  ]
+	}
+  },
+  methods: {
+    addRow() {
+	  this.addWordList.push({word: "", definition: ""})
 	}
   }
 };
