60f2dd5a6c44bdb1f0b2153bfd6fa750c51a0174
B05705055 <46625261+B05705055@users.noreply.github.com>
2020-06-08 20:43:53 +0800

    fini

--- frontend/src/components/page/Navbar.vue
+++ frontend/src/components/page/Navbar.vue
@@ -69,6 +69,9 @@ export default {
           case '/myWord':
             this.defaultActive = '2';
             break;
+		  case '/test':
+            this.defaultActive = '2';
+            break;
 		  case '/addWord':
             this.defaultActive = '2';
             break;
